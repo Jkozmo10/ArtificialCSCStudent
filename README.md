742a9a011cb379583fe0ad92168057ee5cefb227
Nathan McCutchen <nmccutchen@Nathans-MBP.attlocal.net>
2023-12-10 21:29:16 -0800

    alpaca setup & readme

--- README.md
+++ README.md
@@ -1,3 +1,90 @@
-# CSC482-Project
+# Resumator
 
-<a href="https://drive.google.com/uc?export=download&id=1HMBBW5lwmhJCn9x0NGrDZdMo5U8j_eiy" target="_blank">Download the LLM weights</a>
+Resumator is a project created by [Nathan McCutchen](https://github.com/N8WM) ..., for **CSC 482** &mdash; *Speech and Language Processing* at Cal Poly.
+
+## Setup
+
+This project requires Python 3.8+ and a system running MacOS or Linux. If you do not have Python 3.8 installed, you can download it [here](https://www.python.org/downloads/).
+
+### Clone the repository
+
+First clone the repository with the `git clone` command:
+
+```sh
+$ git clone https://github.com/RG-mania/CSC482-Project.git
+```
+
+Then `cd` into the repository:
+
+```sh
+$ cd CSC482-Project
+```
+
+### Create a virtual environment
+
+This step is optional, but recommended. To create a virtual environment, run the following command:
+
+```sh
+$ mkdir .venv && python3 -m venv .venv
+```
+
+Then activate the virtual environment:
+
+```sh
+$ source .venv/bin/activate
+```
+
+### Install Python dependencies
+
+```sh
+$ pip install -r requirements.txt
+```
+
+### Download the LLM weights
+
+```sh
+$ python3 -c "from llm import alpaca; alpaca.download()"
+```
+
+## How to Run the Resume Generator (GUI/CLI)
+
+Our project has both graphical and a command line interfaces. It can be run from either, but the GUI may have some bugs.
+
+### Running the GUI
+
+The GUI requires two separate backend and frontend processes to be running simultaneously.
+
+The backend can be run with the following command:
+
+```sh
+$ python3 ...
+```
+
+To run the frontend, open a new terminal window, `cd` into the project directory, and run the following command:
+
+```sh
+$ python3 ...
+```
+
+With both both the backend and frontend running, navigate to http://localhost:5000 in your browser and you should see the GUI.
+
+Continue explaining the GUI...
+
+### Running the CLI
+
+Description...
+
+```sh
+$ python3 ...
+```
+
+## About the Project
+
+Description...
+
+## Resources
+
+Our project uses a combination of open source libraries and tools:
+
+- [antimatter15/alpaca.cpp](https://github.com/antimatter15/alpaca.cpp) (forked from [ggerganov/llama.cpp](https://github.com/ggerganov/llama.cpp)) &mdash; Alpaca language model
+- [Sosaka/Alpaca-native-4bit-ggml](https://huggingface.co/Sosaka/Alpaca-native-4bit-ggml) &mdash; Alpaca language model weights
--- llm/alpaca.py
+++ llm/alpaca.py
@@ -6,50 +6,13 @@ import os
 import sys
 
 from time import time
-from enum import Enum
-
 from nltk.tokenize import sent_tokenize
+from gdown import download as gdownload
+
 from llm import resources_dir, examples_dir
-
-EXPERIENCES_PURPOSE = """
-You are a resume generator assistant for a Computer Science student at 
-California Polytechnic State University. You accept a company name and 
-a position, and a student academic year, and you are expected to generate 
-bullet points for the student's resume that is most likely to describe 
-their experience in that position. Always start each bullet point with a 
-verb ending in -ed. Put all bullet points into space separated sentences. 
-Always produce an output, even if the input is invalid. Don't use any of
-the example bullet points in the output.
-"""
-
-PROJECTS_NAME_PURPOSE = """
-You are a resue generator assistant for a Computer Science student at 
-California Polytechnic State University. You accept a student academic 
-year, and you are expected to generate the name of a unique project that 
-the student in question could realistically have participated in, as well 
-as the tools used to during the project. Always produce an output, even if 
-the input is invalid. IMPORTANT: Just include "[project_name] - [...tools]" 
-in the output, not an entire sentence. For example, "Discord Bot for the 
-Cal Poly CS Discord Server - Python, Discord.js" is a valid output. Don't 
-use any of the example project names in the output.
-"""
-
-PROJECTS_BULLET_PURPOSE = """
-You are a resue generator assistant for a Computer Science student at 
-California Polytechnic State University. You accept a student academic 
-year and the name of a project + tools, and you are expected to generate 
-bullet points for the student's resume that is most likely to describe what 
-they did for that project. Always start each bullet point with a verb 
-ending in -ed. Put all bullet points into space separated sentences. Always 
-produce an output, even if the input is invalid. Don't use any of the
-example bullet points in the output.
-"""
-
-
-class BulletType(Enum):
-    """ Enum for the type of bullet point """
-    EXPERIENCE = 1
-    PROJECT = 2
+from llm.helper import CHAT_FILE, BulletType
+from llm.prompts import EXPERIENCES_PURPOSE, PROJECTS_NAME_PURPOSE, \
+    PROJECTS_BULLET_PURPOSE
 
 
 class Alpaca:
@@ -59,7 +22,7 @@ class Alpaca:
         self,
         purpose: str,
         example_path: str,
-        llm_path: str,
+        llm_path: str = CHAT_FILE,
         model_path: str = "ggml-alpaca-7b-q4.bin"
     ):
         """
@@ -87,8 +50,10 @@ class Alpaca:
                f"# Example Problems:\n\n{self.examples}\n\n" \
                f"# Real Problem:\n\nInput:\n{input_str}\nOutput:"
 
-    def prompt(self, prompt: str, temp=1, n_threads=12) -> str:
+    def prompt(self, prompt: str, temp=1, n_threads=None) -> str:
         """ Prompt the LLM with a string and return the response """
+        if n_threads is None:
+            n_threads = os.cpu_count() - 1
         with open("prompt.txt", "w", encoding="utf-8") as prompt_file:
             prompt_file.write(prompt)
         return os.popen(
@@ -101,7 +66,7 @@ class Alpaca:
 def generate_exp_bps(company: str, position: str, student_year: str) -> list:
     """ Generate experience bullet points for a resume """
     purpose = EXPERIENCES_PURPOSE
-    llm = Alpaca(purpose, "exp_examples.txt", "chat")
+    llm = Alpaca(purpose, "exp_examples.txt")
     input_str = f'```\n' \
                 f'{{\n' \
                 f'    "company": "{company}",\n' \
@@ -117,8 +82,8 @@ def generate_prj_bps(student_year: str) -> list:
     """ Generate project bullet points for a resume """
     name_purpose = PROJECTS_NAME_PURPOSE
     bullet_purpose = PROJECTS_BULLET_PURPOSE
-    name_llm = Alpaca(name_purpose, "prj_name_examples.txt", "chat")
-    bullet_llm = Alpaca(bullet_purpose, "prj_bullet_examples.txt", "chat")
+    name_llm = Alpaca(name_purpose, "prj_name_examples.txt")
+    bullet_llm = Alpaca(bullet_purpose, "prj_bullet_examples.txt")
     input_str = f'```\n' \
                 f'{{\n' \
                 f'    "studentYear": "{student_year}"\n' \
@@ -167,6 +132,21 @@ def test():
         except KeyboardInterrupt:
             print()
             break
+
+
+def download():
+    """ Downloads the LLM's weights """
+    download_path = os.path.join(resources_dir, "ggml-alpaca-7b-q4.bin")
+    g_drive_id = "1HMBBW5lwmhJCn9x0NGrDZdMo5U8j_eiy"
+    if os.path.exists(download_path):
+        print("Model already downloaded")
+        return
+    try:
+        gdownload(id=g_drive_id, output=download_path, resume=True)
+        print("Done")
+    except KeyboardInterrupt:
+        print(flush=True)
+        print("Download paused. Please run again to resume.")
 
 
 if __name__ == "__main__":
